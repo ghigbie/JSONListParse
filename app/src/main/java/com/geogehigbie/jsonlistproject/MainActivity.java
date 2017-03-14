d571b3aa904a2940f4c29136c1329288bcd97f11
George Higbie <gehigbie@gmail.com>
2017-03-13 19:52:17 -0700

    added variables

--- app/src/main/java/com/geogehigbie/jsonlistproject/MainActivity.java
+++ app/src/main/java/com/geogehigbie/jsonlistproject/MainActivity.java
@@ -3,11 +3,25 @@ package com.geogehigbie.jsonlistproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String URL_BASE = "https://s3.amazonaws.com/technical-challenge/Contacts_v2.json";
+    private ArrayList<String> entries;
+    private ArrayList<String> namesArrayList;
+    private ArrayList<String> mobileArrayList;
+    private final String TAG = "DEBUGGING";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void makeVolleyReqest(){
+
     }
 }
