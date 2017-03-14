adcdb6887e62223ca8c358e8cc227faaae2f7e43
George Higbie <gehigbie@gmail.com>
2017-03-14 01:17:21 -0700

    pulled data from bundle

--- app/src/main/java/com/geogehigbie/jsonlistproject/ContactDetail.java
+++ app/src/main/java/com/geogehigbie/jsonlistproject/ContactDetail.java
@@ -5,21 +5,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 
 public class ContactDetail extends Fragment {
 
     private View view;
+    private String name;
+    private String email;
+    private String birthday;
+    private String company;
+    private String city;
+    private String state;
+    private String street;
+    private String zip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_contact_detail, container, false);
-
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            name = bundle.getString("name");
+            email = bundle.getString("email");
+            birthday = bundle.getString("birthday");
+            company = bundle.getString("company");
+            city = bundle.getString("city");
+            state = bundle.getString("state");
+            street = bundle.getString("street");
+            zip = bundle.getString("zip");
+        }
 
         return view;
     }
+
+    public void setAllTextViews(){}
+
+    TextView nameText = (TextView) view.findViewById(R.id.name_text);
+    TextView emailText = (TextView) view.findViewById(R.id.email_text);
+    TextView birtdayText = (TextView) view.findViewById(R.id.birthday_text);
+    TextView companyText = (TextView) view.findViewById(R.id.company_text);
+    TextView cityText = (TextView) view.findViewById(R.id.city_text);
+    TextView stateText = (TextView) view.findViewById(R.id.state_text);
+    TextView streetText = (TextView) view.findViewById(R.id.street_text);
+    TextView zipText = (TextView) view.findViewById(R.id.zip_text);
+
+
+
+
 
 
 }
